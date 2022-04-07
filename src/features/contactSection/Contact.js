b6594fd71efb00472518e132ae7fec85816c4d07
Isaac Adam <isaacadam04@gmail.com>
2022-04-07 20:07:08 +0300

    Fix: Fixed Some Bugs

--- src/features/contactSection/Contact.js
+++ src/features/contactSection/Contact.js
@@ -6,25 +6,24 @@ import ContactForm from '../../app/components/ContactForm';
 
 const Contact = () => {
   return (
-    <Container fluid>
+    <Container fluid className="py-2">
       <Row>
-        <Col md={7}>
+        <Col md={{ span: 4, offset: 1 }}>
           <h6 className="display-6 text-center py-4">Get In Touch With Us</h6>
           <p className="text-muted text-center font-monospace">
             We Like To Hear From You
           </p>
           <ContactForm />
         </Col>
-        <Col md={5}>
+        <Col md={{ span: 4, offset: 1 }}>
           <div className="contact-info py-4">
             <h3 className="display-6">Reach Us Here:</h3>
             <div className="py-3">
               <p className="lead contact-text">
-                <p>
-                  <i className="fa fa-phone px-3" /> 0714 191789 <br />
-                  <i className="fa fa-map-marker px-3" /> Tabata Aroma <br />
-                  <i className="fa fa-envelope px-3" /> Tabata Aroma
-                </p>
+                <i className="fa fa-phone px-3" /> 0714 191789 <br />
+                <i className="fa fa-map-marker px-3" /> Tabata Aroma <br />
+                <i className="fa fa-envelope px-3" /> sales@nalugeneral.co.tz
+                <br />
                 Nalu General Enterprises <br />
                 P.O Box 24822 <br />
                 Dar es salaam
--- src/features/footer/Footer.js
+++ src/features/footer/Footer.js
@@ -1,7 +1,40 @@
 import React from 'react';
+import { Container, Row, Col, ListGroup } from 'react-bootstrap';
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <Container fluid className="footer-section">
+      <Row>
+        <Col md={4} className="about-us">
+          <h3 className="h3 text-center py-4">About Us</h3>
+          <p className="p-3">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nulla
+            dolorem adipisci quia debitis corporis omnis quidem temporibus,
+            culpa, non illum, voluptas eveniet ab voluptatum! Quos id
+            reprehenderit exercitationem enim dolore.
+          </p>
+          <p className="p-3">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+            Laboriosam, itaque.
+          </p>
+        </Col>
+        <Col md={4}>
+          <h3 className="h3 text-center py-4">Our Partners</h3>
+        </Col>
+        <Col md={4}>
+          <h3 className="h3 text-center py-4">Our Services</h3>
+          <ul>
+            <li>Service 1</li>
+            <li>Service 2</li>
+            <li>Service 3</li>
+            <li>Service 4</li>
+            <li>Service 5</li>
+            <li>Service 6</li>
+          </ul>
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default Footer;
